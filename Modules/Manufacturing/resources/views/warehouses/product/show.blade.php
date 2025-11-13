bfb95bc3cda64c8e9cf08536f35745a85be21f02
moshtaqAlwtr <moshtaqkaid6@gmail.com>
2025-11-13 20:29:02 +0300

    Update product show page action links to use routes
    Replaced static href attributes in the edit and back buttons with dynamic route helpers for better navigation and maintainability.

--- Modules/Manufacturing/resources/views/warehouses/product/show.blade.php
+++ Modules/Manufacturing/resources/views/warehouses/product/show.blade.php
@@ -27,14 +27,14 @@
                     </div>
                 </div>
                 <div class="header-actions">
-                    <a href="#" class="btn btn-edit">
+                    <a href="{{ route('manufacturing.warehouse-products.edit', 1) }}" class="btn btn-edit">
                         <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                             <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
                             <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
                         </svg>
                         تعديل
                     </a>
-                    <a href="#" class="btn btn-back">
+                    <a href="{{ route('manufacturing.warehouse-products.index') }}" class="btn btn-back">
                         <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                             <line x1="19" y1="12" x2="5" y2="12"></line>
                             <polyline points="12 19 5 12 12 5"></polyline>
