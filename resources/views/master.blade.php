9129be7726f5e6bee099ab667e5a11f584aac5f8
moshtaqAlwtr <moshtaqkaid6@gmail.com>
2025-11-20 18:05:15 +0300

    Uncomment dashboard CSS links in master layout
    Restores the inclusion of reports-theme.css, manufacturing-reports.css, and shift-dashboard.css in the master Blade template by uncommenting their link tags. This ensures the relevant styles are loaded for dashboard-related pages.

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -17,11 +17,11 @@
     <link rel="stylesheet" href="{{ asset('assets/fonts/feather/iconfont.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/stages-common.css') }}">
 
-{{--
-    <!-- Dashboard CSS -->
+
+
     <link rel="stylesheet" href="{{ asset('assets/css/reports-theme.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/manufacturing-reports.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/shift-dashboard.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/shift-dashboard.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style-index.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style-add.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/dashboard.css') }}">
